736f4e99e0d6f849d315ffb8676d3bd08be081dd
Michael Pavelchuk <error2268@gmail.com>
2019-02-06 00:19:14 +0700

    Add files via upload

--- FLX_homework_6/homework/js/task1.js
+++ FLX_homework_6/homework/js/task1.js
@@ -1,26 +1,25 @@
-"use strict";
 function checkInput(userInput) {
-    return !(userInput === '' || isNaN(userInput));
+  return !(userInput === '' || isNaN(userInput));
 }
 
-function answer( a, b, dis ) {
-    if (dis > 0) {
-      alert((-b + Math.sqrt(dis)) / (2 * a));
-      alert((-b - Math.sqrt(dis)) / (2 * a));
-    } else if (dis === 0) {
-      alert(-b/(2 * a));
-    } else {
-      alert('no solution (when Discriminant < 0)');
-    }
+function answer(a, b, dis) {
+  if (dis > 0) {
+    alert((-b + Math.sqrt(dis)) / (2 * a));
+    alert((-b - Math.sqrt(dis)) / (2 * a));
+  } else if (dis === 0) {
+    alert(-b / (2 * a));
+  } else {
+    alert('no solution (when Discriminant < 0)');
+  }
 }
 
 var a = +prompt('Input first variable');
 var b = +prompt('Input second variable');
 var c = +prompt('Input third variable');
 
-if ( checkInput(a) && checkInput(b) && checkInput(c) ) {
+if (checkInput(a) && checkInput(b) && checkInput(c)) {
   var dis = Math.pow(b, 2) - 4 * a * c;
   answer(a, b, dis);
-    } else {
-      alert('Invalid input date');
+} else {
+  alert('Invalid input date');
 }
--- FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/homework/js/task2.js
@@ -2,18 +2,18 @@ function checkInput(userInput) {
     return userInput === '' || isNaN(userInput);
 }
 
+function count(money, discount) {
+    if (money < 0 || money > 99999 || discount < 0 || discount > 99 || checkInput(money) || checkInput(discount)) {
+        alert('Invalid data');
+    } else {
+        var discountPrice = +((money * discount) / 100).toFixed(2);
+        var saved = +(money - discountPrice).toFixed(2);
+        alert('Price without discount: ' + money + '\n\rDiscount: ' + discount + '%' +
+            '\n\rPrice with discount: ' + discountPrice + '\n\rSaved: ' + saved);
+    }
+}
+
 var money = +prompt('Input price');
 var discount = +prompt('Input discount');
 
-function count(money, discount) {
-    if (money < 0 || money > 99999 || discount < 0 || discount > 99 || checkInput(money) || checkInput(discount)) {
-    alert('Invalid input data');
-    } else {
-        var discountPrice = +((money * discount)/100).toFixed(2); 
-        var saved = money - discountPrice;
-        alert('Price without discount: ' + money + '\n\rDiscount: ' + discount + '%' +
-              '\n\rPrice with discount: ' + discountPrice + '\n\rSaved: ' + saved);
-    }
-}
 count(money, discount);
-
